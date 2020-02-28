6e24e7795dd8982a8df29f42cd6aa5ddd2814115
Kazi Lotus <kazilotus@hotmail.com>
2020-02-28 12:31:25 +0600

    Google Analytics Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import './App.css';
 
 import Typewriter from './components/Typewriter'
 
 function App() {
+
+  // Initialize GA
+  ReactGA.initialize('UA-54793468-7');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+  
   return (
     <div className="App">
       <header className="App-header">
